7745beddfe8add8d45e267e078254731bb018660
Matthias Nötzli <matthias.noetzli@proton.me>
2025-10-25 21:53:33 +0200

    Update vite.config.ts for supabase

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/lorcana-api': {
-        target: 'https://lorcanajson.org',
+        target: 'https://qdqauljbsttstpolacua.supabase.co/functions/v1/lorcanajson-proxy',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/lorcana-api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkcWF1bGpic3R0c3Rwb2xhY3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MTAzNjMsImV4cCI6MjA3Njk4NjM2M30.euW_DhGdeEy1UbXxY-GsRhBPieEC68g1OArVk9a1biI');
+          });
+        },
       },
     },
   },
