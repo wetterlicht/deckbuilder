902de2056a8867115502925c6cb32760bfdc70a7
Matthias Nötzli <matthias.noetzli@proton.me>
2025-10-26 09:21:42 +0100

    PWA stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,37 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Inkbox',
+        short_name: 'Inkbox',
+        description: 'Inkbox',
+        theme_color: '#373b70',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        orientation: 'portrait',
+      }
+    })
     // vueDevTools(),
   ],
   resolve: {
